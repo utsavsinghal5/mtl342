0b03b24c931e6478e2ff7f19365faf2d06f95e21
Utsav Singhal <utsavsinghal5@gmail.com>
2019-07-31 16:27:16 +0530

    Added basic tree traversal

---
+++ threadedbst.py
@@ -0,0 +1,34 @@
+class Node:
+	def __init__(self,key):
+		self.left = None
+		self.right = None
+		self.val = key
+
+def printInorder(root):
+
+	if root:
+
+		printInorder(root.left)
+		print(root.val),
+		printInorder(root.right)
+
+def printPostorder(root):
+
+	if root:
+
+		printPostorder(root.left)
+		printPostorder(root.right)
+		print(root.val),
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print ("inorder traversal is")
+printInorder(root)
+
+print ("postorder traversal is")
+printPostorder(root)
+
