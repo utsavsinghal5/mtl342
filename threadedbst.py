845040bd9d152b506a483160d58b63647119933e
Utsav Singhal <utsavsinghal5@gmail.com>
2019-07-31 16:43:57 +0530

    most stupid implementation of threaded

--- threadedbst.py
+++ threadedbst.py
@@ -2,6 +2,7 @@ class Node:
 	def __init__(self,key):
 		self.left = None
 		self.right = None
+		self.right1 = None
 		self.val = key
 
 def printInorder(root):
@@ -9,7 +10,7 @@ def printInorder(root):
 	if root:
 
 		printInorder(root.left)
-		print(root.val),
+		print(root.val)
 		printInorder(root.right)
 
 def printPostorder(root):
@@ -18,17 +19,37 @@ def printPostorder(root):
 
 		printPostorder(root.left)
 		printPostorder(root.right)
-		print(root.val),
+		print(root.val)
+
+def threadedInordered(root):
+
+	if root:
+		print(root.val)
+		threadedInordered(root.right1)
+
+
+
+
 
 root = Node(1)
 root.left = Node(2)
 root.right = Node(3)
 root.left.left = Node(4)
 root.left.right = Node(5)
+#root.left.left.right = root.left #This is the extra threads
+#root.left.right.right = root
+root.left.left.right1 = root.left
+root.left.right1 = root.left.right
+root.left.right.right1 = root
+root.right1 = root.right
 
-print ("inorder traversal is")
+print ("inorder traversal is(recursion)")
 printInorder(root)
 
 print ("postorder traversal is")
 printPostorder(root)
+
+print("inorder traversal without recursion")
+threadedInordered(root.left.left)
+
 
